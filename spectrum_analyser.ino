8cb65fd79f1e68e881f9d4efad494c76cacccc3d
Sacha Wheeler <sacha@sachawheeler.com>
2019-04-06 20:20:54 +0100

    install matrix libraries

--- spectrum_analyser.ino
+++ spectrum_analyser.ino
@@ -1,3 +1,24 @@
+/*
+ *  references:
+ *
+ *  spectrum shield
+ *  https://learn.sparkfun.com/tutorials/spectrum-shield-hookup-guide
+ *  https://www.szynalski.com/tone-generator/
+ *
+ *  16x32 led grid setup
+ *  https://learn.adafruit.com/32x16-32x32-rgb-led-matrix/new-wiring
+ *  https://learn.adafruit.com/32x16-32x32-rgb-led-matrix/connecting-with-jumper-wires
+ *
+ *  logic
+ *  https://github.com/snskreationz/Real-Time-RGB-Matrix-Spectrum-Analyzer/blob/master/arduino_code_real_time_spectrum_analyzer.ino
+ *  https://learn.adafruit.com/32x16-32x32-rgb-led-matrix/test-example-code
+ *
+ */
+
+#include <Adafruit_GFX.h>   // Core graphics library
+#include <RGBmatrixPanel.h> // Hardware-specific library
+#include <SPI.h>
+
 //Declare Spectrum Shield pin connections
 #define STROBE 4
 #define RESET 5
@@ -6,17 +27,28 @@
 
 int freq_amp;
 int Frequencies_Left[7];
-int Frequencies_Right[7]; 
+int Frequencies_Right[7];
 int i;
+
+// matrix connections
+#define CLK 8  // MUST be on PORTB!
+#define LAT A3
+#define OE  9
+#define A   A0 // move this
+#define B   A1 // move this
+#define C   A2
+// RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, true);
+
 
 void setup() {
   Serial.begin(9600);
+  // matrix.begin();  
 
   //Set spectrum Shield pin configurations
   pinMode(STROBE, OUTPUT);
   pinMode(RESET, OUTPUT);
   pinMode(DC_One, INPUT);
-  pinMode(DC_Two, INPUT);  
+  pinMode(DC_Two, INPUT);
   digitalWrite(STROBE, HIGH);
   digitalWrite(RESET, HIGH);
 
@@ -33,40 +65,93 @@ void setup() {
 }
 
 /*************Pull frquencies from Spectrum Shield****************/
-void Read_Frequencies(){
+void ReadFrequencies() {
   //Read frequencies for each band
-  for (freq_amp = 0; freq_amp<7; freq_amp++)
+  for (freq_amp = 0; freq_amp < 7; freq_amp++)
   {
     Frequencies_Left[freq_amp] = analogRead(DC_One);
-    Frequencies_Right[freq_amp] = analogRead(DC_Two); 
+    Frequencies_Right[freq_amp] = analogRead(DC_Two);
     digitalWrite(STROBE, HIGH);
     digitalWrite(STROBE, LOW);
   }
 }
 
-void Graph_Frequencies(){
-   Serial.print("F1:   ");
-   for( i= 0; i<7; i++)
-   {
-     Serial.print(Frequencies_Left[i]);
-     Serial.print("  ");
-     
-   }
-   Serial.println();
-   Serial.print("F2:   ");
-   for( i= 0; i<7; i++)
-   {
-     Serial.print(Frequencies_Right[i]);
-     Serial.print("  ");
-     
-   }
-   Serial.println();
+void SerialOutput() {
+  Serial.print("F1:   ");
+  for ( i = 0; i < 7; i++)
+  {
+    Serial.print(Frequencies_Left[i]);
+    Serial.print("  ");
+
+  }
+  Serial.println();
+  Serial.print("F2:   ");
+  for ( i = 0; i < 7; i++)
+  {
+    Serial.print(Frequencies_Right[i]);
+    Serial.print("  ");
+
+  }
+  Serial.println();
 }
+
+void PlotFrequencies(){
+    
+}
+
+void lightcolumns(int rownum, int amplitude)
+{
+  if (amplitude > 15) // <-O-> set the threshold for the band to turn red
+  {
+    for ( int y = 0; y < amplitude; y++) {
+      matrix.drawPixel(rownum, y, matrix.Color333(7, 0, 0));
+    }
+    for (int y = amplitude; y < 16; y++)
+    {
+      matrix.drawPixel(rownum, y, matrix.Color333(0, 0, 0));
+    }
+  }
+
+  else if (amplitude > 13) // <-O-> set the threshold for the band to turn yellow
+  {
+    for ( int y = 0; y < amplitude; y++) {
+      matrix.drawPixel(rownum, y, matrix.Color333(4, 4, 0));
+    }
+    for (int y = amplitude; y < 16; y++)
+    {
+      matrix.drawPixel(rownum, y, matrix.Color333(0, 0, 0));
+    }
+  }
+
+  else if (amplitude > 9) // <-O-> set the threshold for the band to turn green
+  {
+    for ( int y = 0; y < amplitude; y++) {
+      matrix.drawPixel(rownum, y, matrix.Color333(0, 5, 0));
+    }
+    for (int y = amplitude; y < 16; y++)
+    {
+      matrix.drawPixel(rownum, y, matrix.Color333(0, 0, 0));
+    }
+  }
+
+  else
+  {
+    for ( int y = 0; y < amplitude; y++) {
+      matrix.drawPixel(rownum, y, matrix.Color333(0, 0, 7));
+    }
+    for (int y = amplitude; y < 16; y++)
+    {
+      matrix.drawPixel(rownum, y, matrix.Color333(0, 0, 0));
+    }
+  }
+}
+
 
 void loop() {
 
-  Read_Frequencies();
-  Graph_Frequencies();
+  ReadFrequencies();
+  SerialOutput();
+  PlotFrequencies();
   delay(1500);
 
 }
