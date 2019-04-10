84c95dfa6bced238e0210f681375190f2f0ee7bd
Sacha Wheeler <sacha@sachawheeler.com>
2019-04-10 13:40:52 +0100

    pretty much done

--- spectrum_analyser.ino
+++ spectrum_analyser.ino
@@ -25,33 +25,29 @@
 #define CHANNEL_ONE  A5 // A1 moved
 #define CHANNEL_TWO A4 // A0 moved
 
-int freq_amp;
-int freq_left[7];
-int freq_right[7];
-int i;
-int amp_max;
+unsigned int freq_left[7];
+unsigned int freq_right[7];
+unsigned int amp_max;
 
-int RED_THRESHOLD = 13;
-int YELLOW_THRESHOLD = 9;
-int GREEN_THRESHOLD = 5;
-int row_max[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+#define RED_THRESHOLD 13
+#define YELLOW_THRESHOLD 9
+#define GREEN_THRESHOLD 5
 
 // matrix connections
-#define CLK 8  // MUST be on PORTB!
-#define LAT 10 // LAT == STROBE
-#define OE  9
 #define A   A0
 #define B   A1
 #define C   A2
-RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
-
-int mode = 0;
+#define CLK 8  // MUST be on PORTB!
+#define LAT 10 // LAT == STROBE
+#define OE  9
+const bool double_buffering = false;
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, double_buffering); // last argument is for double-buffering
 
 // sensitivity
-int sensitivity = 2; // this could come from a pot
-const int MAX_SENSITIVITY = 12;
+unsigned int sensitivity = 2; // this could come from a pot
+const unsigned int MAX_SENSITIVITY = 12;
 unsigned long previousMillis = 0;
-const long interval = 1400;
+const unsigned int interval = 1400;
 
 void setup() {
   Serial.begin(9600);
@@ -75,13 +71,12 @@ void setup() {
   digitalWrite(STROBE, LOW);
   delay(1);
   digitalWrite(RESET, LOW);
-
 }
 
 /*************Pull frquencies from Spectrum Shield****************/
 void ReadFrequencies() {
   //Read frequencies for each band
-  for (freq_amp = 0; freq_amp < 7; freq_amp++)
+  for (unsigned int freq_amp = 0; freq_amp < 7; freq_amp++)
   {
     freq_left[freq_amp] = analogRead(CHANNEL_ONE);
     freq_right[freq_amp] = analogRead(CHANNEL_TWO);
@@ -92,14 +87,14 @@ void ReadFrequencies() {
 
 void SerialOutput() {
   Serial.print("Left:   ");
-  for ( i = 0; i < 7; i++)
+  for (int i = 0; i < 7; i++)
   {
     Serial.print(freq_left[i]);
     Serial.print("  ");
   }
   Serial.println();
   Serial.print("Right:   ");
-  for ( i = 0; i < 7; i++)
+  for (int i = 0; i < 7; i++)
   {
     Serial.print(freq_right[i]);
     Serial.print("  ");
@@ -110,18 +105,18 @@ void SerialOutput() {
 void PlotFrequencies() {
 
   // lightcolumns(0, 0);
-  int left_total = 0;
-  int right_total = 0;
+  unsigned int left_total = 0;
+  unsigned int right_total = 0;
 
-  for (int i = 1; i < 15; i += 2) {
-    int idx = (i - 1) / 2;
+  for (unsigned int i = 1; i < 15; i += 2) {
+    unsigned int idx = (i - 1) / 2;
     lightcolumns(i, freq_left[idx]);
     lightcolumns(i + 1, freq_left[idx]);
     left_total += freq_left[idx];
   }
 
-  for (int j = 17; j < 31; j += 2) {
-    int idx = (j - 17) / 2;
+  for (unsigned int j = 17; j < 31; j += 2) {
+    unsigned int idx = (j - 17) / 2;
     lightcolumns(j, freq_right[idx]);
     lightcolumns(j + 1, freq_right[idx]);
     right_total += freq_right[idx];
@@ -131,26 +126,29 @@ void PlotFrequencies() {
   lightcolumns(16, int(right_total / 7 ));
 
   // lightcolumns(31, sensitivity - 1);
-  for ( int k = 0; k < 16; k++) {
+  for (unsigned int k = 0; k < 16; k++) {
     if (k == sensitivity)
       matrix.drawPixel(31, k, matrix.Color333(7, 7, 7));
     else
       matrix.drawPixel(31, k, matrix.Color333(0, 0, 0));
   }
 
-  matrix.swapBuffers(false);
+  if (double_buffering)
+    matrix.swapBuffers(false); // we're only able to use single-buffering due to SRAM limitations
 }
 
-void lightcolumns(int row_num, int amp_1024)
+void lightcolumns(unsigned int row_num, unsigned int amp_1024)
 {
-  if (amp_1024 > 32) amp_1024 -= 32;  // trim some low end noise
-  else amp_1024 = 0;
+  if (amp_1024 > 32)
+    amp_1024 -= 32;  // trim some low end noise
+  else
+    amp_1024 = 0;
+  unsigned int amplitude = int((amp_1024 * sensitivity) / 63.5);
 
-  int amplitude = int((amp_1024 * sensitivity) / 63.5);
+  if (amplitude > amp_max && amplitude < 16)
+    amp_max = amplitude;
 
-  if (amplitude > amp_max && amplitude < 16) amp_max = amplitude;
-
-  for ( int y = 0; y < 16; y++) {
+  for (unsigned int y = 0; y < 16; y++) {
 
     if (row_num == 15 || row_num == 16) {
       if (y == amplitude)
@@ -178,6 +176,11 @@ void lightcolumns(int row_num, int amp_1024)
   }
 }
 
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
 
 void loop() {
   unsigned long currentMillis = millis();
@@ -199,5 +202,7 @@ void loop() {
   // SerialOutput();
   PlotFrequencies();
 
-  delay(30);
+  delay(20);
+  int ram = freeRam();
+  Serial.println(ram);
 }
